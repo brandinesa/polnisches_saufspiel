f14f6c32a4adde27840fa83b22d1c12aacdff54f
brandinesa <meinpatrick@gmx.at>
2017-07-07 17:46:42 +0200

    Initial project setup

---
+++ src/main/java/inc/alc/polish/PolnischesSaufspielApplication.java
@@ -0,0 +1,12 @@
+package inc.alc.polish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolnischesSaufspielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolnischesSaufspielApplication.class, args);
+	}
+}
